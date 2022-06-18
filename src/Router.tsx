d8e51ccfaf8627928b783d3498c37f55cde2b37a
박지윤 <jyun1937@naver.com>
2022-06-18 16:36:09 +0900

    Route path change

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import Slider from "./Slider";
 
 function Router() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="FramerDemo" element={<FramerPractice />} />
