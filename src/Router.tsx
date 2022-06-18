1440f5f5004a0043490f118c0a0ad37617685fc2
박지윤 <jyun1937@naver.com>
2022-06-18 14:55:44 +0900

    change path

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,16 +8,10 @@ function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="animation-practice/*" element={<Home />} />
-				<Route
-					path="animation-practice/FramerDemo"
-					element={<FramerPractice />}
-				/>
-				<Route path="animation-practice/Slider" element={<Slider />} />
-				<Route
-					path="animation-practice/SharedLayout"
-					element={<SharedLayout />}
-				/>
+				<Route path="/" element={<Home />} />
+				<Route path="FramerDemo" element={<FramerPractice />} />
+				<Route path="Slider" element={<Slider />} />
+				<Route path="SharedLayout" element={<SharedLayout />} />
 			</Routes>
 		</BrowserRouter>
 	);
